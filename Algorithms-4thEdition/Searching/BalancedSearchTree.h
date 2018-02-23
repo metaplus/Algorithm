78eb5d3cb71e8ad7afbf576de316dec89aa7c5c5
Jingxing Xu <jx.xu@outlook.com>
2018-02-23 11:58:53 +0800

    BalancedSearchTree searching::red_black_tree<Key,Value,Compare>

---
+++ Algorithms-4thEdition/Searching/BalancedSearchTree.h
@@ -0,0 +1,82 @@
+#pragma once
+#include <functional>
+#include <memory>
+namespace searching
+{
+    template<typename Key, typename Value, typename Compare = std::less<Key>>
+    struct red_black_tree   // height < 2lgN
+    {                       // assume left lean tree for 3-node
+        enum color { red, black };
+        enum direction { left, right };// average path length ~ 1.00lgN
+        struct node
+        {
+            Key key;
+            Value value;
+            size_t count;
+            color color;
+            std::unique_ptr<node> left, right;
+        };
+        std::unique_ptr<node> root;
+        bool is_red(const std::unique_ptr<node>& pnode) const
+        {
+            return pnode && pnode->color == red;
+        }
+        size_t count(const std::unique_ptr<node>& pnode) const
+        {
+            return pnode ? pnode->count : 0;
+        }
+        void rotate(const direction direction, std::unique_ptr<node>& pnode)
+        {   // 3-element swap chain, capture 3 reference + 2 swap operations or exchange manners
+            if (direction == left)
+            {   
+                auto& rtree = pnode->right;
+                auto& rtree_ltree = pnode->right->left; 
+                std::swap(pnode, rtree);
+                std::swap(rtree, rtree_ltree);
+                pnode->left->count = 1 + count(pnode->left->left) + count(pnode->left->right);
+                pnode->count = 1 + count(pnode->left) + count(pnode->right);
+            }
+            else    // right
+            {
+                auto& ltree = pnode->left;
+                auto& ltree_rtree = pnode->left->right;
+                std::swap(pnode, ltree);
+                std::swap(ltree, ltree_rtree);
+                pnode->right->count = 1 + count(pnode->right->left) + count(pnode->right->right);
+                pnode->count = 1 + count(pnode->left) + count(pnode->right);
+            }
+        }
+        void flip(std::unique_ptr<node>& pnode)
+        {
+            pnode->left->color = black;
+            pnode->right->color = black;
+            pnode->color = red;
+        }
+        node& emplace_impl(Key& key, Value& value, std::unique_ptr<node>& pnode)
+        {
+            if(!pnode)
+            {
+                pnode = std::make_unique<node>(node{ std::move(key),std::move(value),1,red });
+                return *pnode;
+            }
+            static const Compare cmp{};
+            if (!cmp(key, pnode->key) && !cmp(pnode->key, key))
+            {
+                pnode->value = std::move(value);
+                return *pnode;
+            }
+            auto& inserted = cmp(key, pnode->key)  ? emplace_impl(key, value, pnode->left) : emplace_impl(key, value, pnode->right);
+            if (!is_red(pnode->left) && is_red(pnode->right)) rotate(left, pnode);
+            if (is_red(pnode->left) && is_red(pnode->left->left)) rotate(right, pnode);
+            if (is_red(pnode->left) && is_red(pnode->right)) flip(pnode);
+            pnode->count = 1 + count(pnode->left) + count(pnode->right);
+            return inserted;
+        }
+        node& emplace(Key key, Value value)
+        {
+            auto& inserted = emplace_impl(key, value, root);
+            root->count = black;
+            return inserted;
+        }
+    };
+}
--- Algorithms-4thEdition/Searching/BinarySearchTree.h
+++ Algorithms-4thEdition/Searching/BinarySearchTree.h
@@ -2,7 +2,7 @@
 #include <functional>
 #include <memory>
 #include <stdexcept>
-namespace seaching
+namespace searching
 {
     template<typename Key, typename Value, typename Compare = std::less<Key>>
     struct binary_search_tree
@@ -15,69 +15,69 @@ namespace seaching
             std::unique_ptr<node> left, right;
         };
         std::unique_ptr<node> root;
-        size_t count(std::unique_ptr<node>& node) const
+        size_t count(std::unique_ptr<node>& pnode) const
         {
-            return node ? node->count : 0;
+            return pnode ? pnode->count : 0;
         }
-        node& emplace_impl(Key& key, Value& value, std::unique_ptr<node>& current)
+        node& emplace_impl(Key& key, Value& value, std::unique_ptr<node>& pnode)
         {
-            if(!current)
+            if(!pnode)
             {
-                current = std::make_unique<node>(node{ std::move(key),std::move(value),1 });
-                return *current;
+                pnode = std::make_unique<node>(node{ std::move(key),std::move(value),1 });
+                return *pnode;
             }
             static const Compare cmp{};
-            if (!cmp(key, current->key) && !cmp(current->key, key))
+            if (!cmp(key, pnode->key) && !cmp(pnode->key, key))
             {
-                current->value = std::move(value);
-                return *current;
+                pnode->value = std::move(value);
+                return *pnode;
             }
-            auto& last = emplace_impl(key, value, cmp(key, current->key) ? current->left : current->right);
-            current->count = 1 + count(current->left) + count(current->right);
+            auto& last = emplace_impl(key, value, cmp(key, pnode->key) ? pnode->left : pnode->right);
+            pnode->count = 1 + count(pnode->left) + count(pnode->right);
             return last;
         }
         node& emplace(Key key, Value value)
         {
             return emplace_impl(key, value, root);
         }
-        Value at_impl(const Key& key, const std::unique_ptr<node>& current) const
+        Value at_impl(const Key& key, const std::unique_ptr<node>& pnode) const
         {
-            if (!current) throw std::invalid_argument{ "no such key" };
+            if (!pnode) throw std::invalid_argument{ "no such key" };
             static const Compare cmp{};
-            if (!cmp(key, current->key) && !cmp(current->key, key))
-                return current->value;
-            return at_impl(key, cmp(key, current->key) ? current->left : current->right);
+            if (!cmp(key, pnode->key) && !cmp(pnode->key, key))
+                return pnode->value;
+            return at_impl(key, cmp(key, pnode->key) ? pnode->left : pnode->right);
         }
         Value at(const Key& key) const
         {
             return at_impl(key, root);
         }
-        std::unique_ptr<node> delete_min_impl(std::unique_ptr<node>& current)
+        std::unique_ptr<node> delete_min_impl(std::unique_ptr<node>& pnode)
         {
-            if (!current)
+            if (!pnode)
                 return {};
-            if (!current->left)
-                return std::exchange(current, std::move(current->right));
-            current->count -= 1;
-            return delete_min_impl(current->left);
+            if (!pnode->left)
+                return std::exchange(pnode, std::move(pnode->right));
+            pnode->count -= 1;
+            return delete_min_impl(pnode->left);
         }
-        std::unique_ptr<node> erase_impl(const Key& key, std::unique_ptr<node>& current)
+        std::unique_ptr<node> erase_impl(const Key& key, std::unique_ptr<node>& pnode)
         {
-            if (!current) throw std::invalid_argument{ "no such key" };
+            if (!pnode) throw std::invalid_argument{ "no such key" };
             static const Compare cmp{};
-            if (cmp(key, current->key))
-                return erase_impl(key, current->left);
-            if (cmp(current->key, key))
-                return erase_impl(key, current->right);
-            if (!current->left)
-                return std::exchange(current, std::move(current->right));
-            if (!current->right)
-                return std::exchange(current, std::move(current->left));
-            auto rmin = delete_min_impl(current->right);
-            rmin->count = current->count - 1;
-            rmin->left = std::move(current->left);          // otherwise directly swap Key,Value
-            rmin->right = std::move(current->right);
-            return std::exchange(current, std::move(rmin));
+            if (cmp(key, pnode->key))
+                return erase_impl(key, pnode->left);
+            if (cmp(pnode->key, key))
+                return erase_impl(key, pnode->right);
+            if (!pnode->left)
+                return std::exchange(pnode, std::move(pnode->right));
+            if (!pnode->right)
+                return std::exchange(pnode, std::move(pnode->left));
+            auto rmin = delete_min_impl(pnode->right);
+            rmin->count = pnode->count - 1;
+            rmin->left = std::move(pnode->left);          // otherwise directly swap Key,Value
+            rmin->right = std::move(pnode->right);
+            return std::exchange(pnode, std::move(rmin));
         }
         std::unique_ptr<node> erase(const Key& key)
         {
